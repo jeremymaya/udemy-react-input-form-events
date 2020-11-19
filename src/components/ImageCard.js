4d555d9f21fcd969ade7cb75d57aa1f47ca4289d
Kyungrae <kyungraekim@outlook.com>
2020-11-19 12:34:13 -0800

    callbacks on image load complete

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,16 +1,32 @@
 import React from 'react';
 
 class ImageCard extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.imageRef = React.createRef();
+    }
+
+    // accesses the imgage ref
+    componentDidMount() {
+        // waits for an image to load 
+        // and paired up with a callback setSpans
+        this.imageRef.current.addEventListener('load', this.setSpans);
+    }
+
+    setSpans = () => {
+        console.log(this.imageRef.current.clientHeight);
+    };
+
     render() {
         // destructuring this.props.image
         const { description, urls } = this.props.image;
 
+        // <img> tag here is a JSX tag which will eventually become a JS element
+        // ref will contain the image node information
         return (
             <div>
-                <img
-                    src={urls.regular}
-                    alt={description}
-                />
+                <img src={urls.regular} alt={description} ref={this.imageRef} />
             </div>
         )
     }
